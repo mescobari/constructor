73ab5d51190d2f24fc75cdc278bd3d2e5e3896cf
Gabriel Max Antonio Escobari Quiroga <gabriel.escobari@EBC.LOCAL>
2022-08-17 16:55:42 -0400

    iniciando reportes

--- resources/js/reportes/Reporte.vue
+++ resources/js/reportes/Reporte.vue
@@ -2,7 +2,7 @@
     <div>        
         <div class="card">
             <div class="card-header ferdy-background-Primary-blak">
-                <h3 class="card-title">REPORTE PLANIFICACIÓN</h3>
+                <h3 class="card-title">Reportes Seguimiento Fisico</h3>
                 <div class="card-tools">
                     <!-- <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#intervencion" @click="ModalCrear();">
                         Crear Cofinanciador
@@ -12,11 +12,14 @@
             <br>
             <div class="card-body">                 
                 <div class="container p-3 border rounded mb-3">
+
+ 
+
                     <div class="row">
                         <div class="col-md-10">          
                             <div class="form-group">
-                                <label for="codsisin">Seleccione Proyecto:</label>
-                                <v-select label="nombre" :options="proyectos" v-model="proyecto" placeholder="Selecione un departamento">
+                                <label for="codsisin">Seleccione Contrato:</label>
+                                <v-select label="nombre" :options="proyectos" v-model="proyecto" placeholder="Selecione un contrato">
                                     <span slot="no-options">No hay datos para cargar</span>
                                 </v-select>
                             </div>
@@ -29,20 +32,78 @@
                         </div>
                     </div>
                 </div>
+
+
+<hr>
+
+<div class="row">
+                    <div class="col-md-12">
+                        <center>
+                             <h3>  {{ proyecto.nombre }}</h3>
+                        </center>
+                    </div>
+                </div>
+
+                 <hr>
+
+
+
+
+
+
+
+
+<div class="row">
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-info">
+                        <div class="inner">
+                            <br><br>
+                            <h4>LIsa de Documentos</h4>
+                            <br>
+                        </div>
+                        <div class="icon">
+                            <i class="far fa-flag"></i>
+                        </div>
+                        <a :href="'mml_proyecto/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                     <div class="col-lg-3 col-6">
+                        <div class="small-box bg-danger">
+                        <div class="inner">
+                            <br><br>
+                            <!-- <h3>65</h3> -->
+                            <h4>Vencimientos</h4>
+                        </div>
+                        <div class="icon">
+                            <i class="far fa-star"></i>
+                        </div>
+                        <a :href="'ficha_proyecto_documentos/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-warning">
+                         </div>
+                    </div>
+                    <div class="col-lg-3 col-6">
+                        <div class="small-box bg-danger">
+                        </div>
+                    </div>
+                </div>
+
+  <!-- / aqui termina Mod MAx -->
+<hr>
                 <div class="row">
                     <div class="col-lg-3 col-6">
                         <div class="small-box bg-info">
                         <div class="inner">
                             <br><br>
-                            <!-- <h3>150</h3> -->
-                            <!-- <h4>Ficha del Proyecto</h4> -->
-                            <h4>Ficha del Proyecto</h4>
+                            <h4>Planilla Vigente</h4>
                             <br>
                         </div>
                         <div class="icon">
                             <i class="far fa-envelope"></i>
                         </div>
-                        <a :href="'ficha_proyecto/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
+                        <a :href="'ver_pla_vigente/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-6">
@@ -50,12 +111,12 @@
                         <div class="inner">
                             <br><br>
                             <!-- <h3>53<sup style="font-size: 20px">%</sup></h3> -->
-                            <h4>Localización del Proyecto</h4>
+                            <h4>Ejecucion Fisica</h4>
                         </div>
                         <div class="icon">
                             <i class="far fa-flag"></i>
                         </div>
-                        <a :href="'ficha_proyecto_localizacion/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
+                        <a :href="'ver_pla_fisico/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
                     <div class="col-lg-3 col-6">
@@ -63,7 +124,7 @@
                         <div class="inner">
                             <br><br>
                             <!-- <h3>44</h3> -->
-                            <h4>Estructura de Financiamiento</h4>
+                            <h4>Inidcadores Ejec. Fisica</h4>
                         </div>
                         <div class="icon">
                             <i class="far fa-copy"></i>
@@ -87,46 +148,7 @@
                 </div>
 
 
- <!-- Desde aqui Mod MAx -->
 
-<div class="row">
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-info">
-                        <div class="inner">
-                            <br><br>
-                            <h4>Matriz del Marco Logico</h4>
-                            <br>
-                        </div>
-                        <div class="icon">
-                            <i class="far fa-flag"></i>
-                        </div>
-                        <a :href="'mml_proyecto/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                     <div class="col-lg-3 col-6">
-                        <div class="small-box bg-danger">
-                        <div class="inner">
-                            <br><br>
-                            <!-- <h3>65</h3> -->
-                            <h4>Repositorio de Documentos</h4>
-                        </div>
-                        <div class="icon">
-                            <i class="far fa-star"></i>
-                        </div>
-                        <a :href="'ficha_proyecto_documentos/'+proyecto.id" target="_blank" class="small-box-footer" v-if="proyecto.id">Ir a reporte <i class="fas fa-arrow-circle-right"></i></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-warning">
-                         </div>
-                    </div>
-                    <div class="col-lg-3 col-6">
-                        <div class="small-box bg-danger">
-                        </div>
-                    </div>
-                </div>
-
-  <!-- / aqui termina Mod MAx -->
 
 
             </div>
@@ -144,8 +166,15 @@ export default {
     },
     methods:{        
         async busca_proyectos(){
-            var respuesta = await axios.get('proyectos_de_institucion_reporte');
-            this.proyectos = respuesta.data;
+            //var respuesta = await axios.get('proyectos_de_institucion_reporte');
+            //this.proyectos = respuesta.data;
+            var respuesta = await axios.get('documents');
+            console.log('===============');
+            console.log(respuesta.data);
+            const principales=respuesta.data.filter((item)=> item.document_types_id===1 )
+            this.proyectos = principales;
+
+
         }
     },
     created(){
--- routes/otras_rutas/front-end/constructor/Reportes.php
+++ routes/otras_rutas/front-end/constructor/Reportes.php
@@ -1,11 +1,13 @@
 <?php
     Route::group(['prefix' => 'paginas', 'namespace' => 'Front_End\Constructor'], function () {
        
-        
-        Route::get('rep_indicadores', 'PlanillaReportesController@inicio')->name('Reportes');  //menu
-        Route::get('ver_planilla/{id}', 'PlanillaReportesController@planilla_individual')->name('planilla_individual');
        
-   
+        
+        Route::get('rep_indicadores', 'PlanillaReportesController@inicio')->name('rep_indicadores');  //menu
+        Route::get('ver_planilla/{id}', 'PlanillaReportesController@planilla_individual')->name('planilla_individual');
+        Route::get('ver_pla_vigente/{id}', 'PlanillaReportesController@planilla_vigente')->name('ver_pla_vigente');
+        Route::get('ver_pla_fisico/{id}', 'PlanillaReportesController@planilla_ejecucion')->name('ver_pla_fisico');
+       
    
     });     
 ?>
