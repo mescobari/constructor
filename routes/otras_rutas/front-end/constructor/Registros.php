be3cebec41f604b69c826a330e843b78c87448ee
mescobari <maxescob@hotmail.com>
2022-08-29 21:13:40 -0400

    esta vez si

--- routes/otras_rutas/front-end/constructor/Registros.php
+++ routes/otras_rutas/front-end/constructor/Registros.php
@@ -34,7 +34,19 @@ Route::group(['prefix' => 'paginas', 'namespace' => 'Front_End\Constructor'], fu
     Route::delete('delete_requerimiento_relacion/{id}', 'RequerimientoController@deleteItemRelacion')->name('delete_requerimiento_relacion');
     Route::delete('delete_requerimiento_otros_gastos/{id}', 'RequerimientoController@deleteItemOtrosGastos')->name('delete_requerimiento_otros_gastos');
     Route::post('update_requerimiento_item/{id}', 'RequerimientoController@updateRequerimientoItem')->name('update_requerimiento_item');
-    
+    /*Route::apiResource('intervenciones', IntervencionesController::class);
+    Route::post('intervenciones_mod', 'IntervencionesController@update')->name('intervenciones_mod');
+    Route::get('proyectos', 'IntervencionesController@proyectos')->name('proyectos');
+//create_requerimiento_otros_gastos
+    Route::get('intervencion_tipo', 'TipoIntervencionesController@inicio')->name('intervencion_tipo');
+    Route::get('intervencions_tipo', 'TipoIntervencionesController@intervencionesTipoActivas')->name('intervencions_tipo');
+    Route::apiResource('intervenciones_tipo', TipoIntervencionesController::class);
+
+    Route::get('sectorial', 'SectorialController@inicio')->name('intervencion_tipo');
+    Route::get('sectorials', 'SectorialController@sectorialesActivas')->name('intervencions_tipo');
+    Route::apiResource('sectoriales', SectorialController::class);
+
+     */
 });
 
 ?>
