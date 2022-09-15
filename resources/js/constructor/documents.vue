85ee766a63ea43e8b46d662370f4d40a151cfc7d
Raikonif <raikonif@gmail.com>
2022-09-15 10:02:15 -0400

    documents.vue modified method editar now show correctly de date, changing to f.(getmoth,getdate,getyear), previous was date,month,year

--- resources/js/constructor/documents.vue
+++ resources/js/constructor/documents.vue
@@ -924,13 +924,14 @@ export default {
             this.jsonData.objeto = data.objeto;
 
             const fecha = new Date(data.fecha_firma);
+            // const formateado = moment(data.fecha_firma, 'D-M-YYYY')
             const formateado = moment(data.fecha_firma, 'D-M-YYYY')
             const dateObject = formateado.toDate();
             const f = new Date(dateObject);
             console.log("FECHA", dateObject);
             // this.jsonData.fecha_firma = fecha.getDate() + '-' + (fecha.getMonth() + 1) + '-' + fecha.getFullYear();
             // this.jsonData.fecha_firma = data.fecha_firma
-            this.jsonData.fecha_firma = (f.getDate() + '-' + (f.getMonth() + 1) + '-' + f.getFullYear()).toString();
+            this.jsonData.fecha_firma = ((f.getMonth() + 1) + '-' + f.getDate() + '-' + f.getFullYear()).toString();
             console.log("FECHA OBTENIDA", this.jsonData.fecha_firma);
 
             this.jsonData.files = data.files;
